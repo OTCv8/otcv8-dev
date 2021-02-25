28edd587463c59626c1b377e090a0745ba79ce03
OTCv8 <otclient@otclient.ovh>
2021-02-26 00:38:31 +0100

    Better tests, including bot, updated vithrax config

--- test.lua
+++ test.lua
@@ -1,10 +1,25 @@
 -- First test
-Test.Test("First test, maximize and hide entergame", function(test, wait, ss, fail)
+Test.Test("First test, maximize window, open options, do screenshot", function(test, wait, ss, fail)
     wait(1000)
     test(function() 
         g_window.maximize()
     end)
     wait(3000)
+    ss()
+    test(function()
+        modules.client_options.show()
+    end)
+    wait(1000)
+    ss()
+    wait(1000)
+    test(function()
+        local optionsWindow = g_ui.getRootWidget():recursiveGetChildById("optionsWindow")
+        if not optionsWindow or optionsWindow:isHidden() then
+            fail("Options window is not opened")
+        end
+        modules.client_options.hide()
+    end)
+    wait(1000)
     ss()
 end)
 
