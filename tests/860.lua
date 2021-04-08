90e877c8c35963f034756fcdd861b65b1f80c613
OTCv8 <otclient@otclient.ovh>
2021-04-08 12:12:16 +0200

    Restored more advanced tests

--- tests/860.lua
+++ tests/860.lua
@@ -1,4 +1,4 @@
-Test.Test("Test 860", function(test, wait, ss, fail)
+Test.Test("Test 860 and bot", function(test, wait, ss, fail)
     test(function()
         EnterGame.hide()
         g_settings.setNode("things", {})
@@ -6,9 +6,44 @@ Test.Test("Test 860", function(test, wait, ss, fail)
         g_game.setProtocolVersion(g_game.getClientProtocolVersion(860))
         g_game.playRecord("860.record")
     end)
-    for i=1,5 do
-        wait(5000)
-        ss()    
+
+    wait(3000)
+    ss()
+
+    local configId = 0
+    for i=1,3 do
+        test(function()
+            local botWindow =  g_ui.getRootWidget():recursiveGetChildById("botWindow")
+            local configs = botWindow:recursiveGetChildById("config")
+            local enableButton = botWindow:recursiveGetChildById("enableButton")
+            local configsCount = configs:getOptionsCount()
+            configId = configId + 1
+            if configId > configsCount then
+                configId = 1
+            end
+            configs:setCurrentIndex(configId)
+            enableButton.onClick()
+        end)
+        wait(1000)
+        ss()
+        for x=1,5 do
+            test(function()
+                local botWindow =  g_ui.getRootWidget():recursiveGetChildById("botWindow")
+                local tabs = botWindow:recursiveGetChildById("botTabs")
+                tabs:selectNextTab()
+            end)
+            wait(500)
+            ss()
+            wait(500)
+        end
+        wait(1000)
+        test(function()
+            local botWindow =  g_ui.getRootWidget():recursiveGetChildById("botWindow")
+            local enableButton = botWindow:recursiveGetChildById("enableButton")
+            enableButton.onClick()
+        end)
+        ss()
+        wait(1000)
     end
 
     test(function()
@@ -32,10 +67,19 @@ Test.Test("Test 860", function(test, wait, ss, fail)
         end
     end)
 
-    for i=1,3 do
-        wait(5000)
-        ss()    
-    end
+    wait(1000)
+    test(function()
+        modules.client_stats.toggle()
+    end)
+    ss()
+    wait(2000)
+    test(function()
+        modules.client_stats.toggle()
+    end)
+    wait(1000)
+    ss()
+    wait(5000)
+    ss()    
 
     test(function()
         if g_game.isOnline() then
