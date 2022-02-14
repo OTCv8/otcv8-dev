c2306e5041b2fd9a0c233950d5b66cad7faab8c6
Vithrax <adrian.gajdulewicz@outlook.com>
2022-02-14 12:35:24 +0100

    added missing images

--- modules/game_shaders/shaders.lua
+++ modules/game_shaders/shaders.lua
@@ -1,22 +1,8 @@
 function init()
   -- add manually your shaders from /data/shaders
 
-  -- map shaders
-  g_shaders.createShader("map_default", "/shaders/map_default_vertex", "/shaders/map_default_fragment")  
-
-  g_shaders.createShader("map_rainbow", "/shaders/map_rainbow_vertex", "/shaders/map_rainbow_fragment")
-  g_shaders.addTexture("map_rainbow", "/images/shaders/rainbow.png")
-
-  -- use modules.game_interface.gameMapPanel:setShader("map_rainbow") to set shader
-
-  -- outfit shaders
-  g_shaders.createOutfitShader("outfit_default", "/shaders/outfit_default_vertex", "/shaders/outfit_default_fragment")
-
-  g_shaders.createOutfitShader("outfit_rainbow", "/shaders/outfit_rainbow_vertex", "/shaders/outfit_rainbow_fragment")
-  g_shaders.addTexture("outfit_rainbow", "/images/shaders/rainbow.png")
-
-  -- you can use creature:setOutfitShader("outfit_rainbow") to set shader
-
+  g_shaders.createOutfitShader("rainbow", "/shaders/outfit_rainbow_vertex", "/shaders/outfit_rainbow_fragment")
+  g_shaders.addTexture("rainbow", "/images/shaders/rainbow.png")  
 end
 
 function terminate()
